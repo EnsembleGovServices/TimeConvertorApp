d7d1aedc1fb39fe499ed365542b65f5c8585b08e
Ensemble Consultancy <73653771+ensemblegov@users.noreply.github.com>
2023-01-24 09:22:42 -0800

    Update class-nasa-time-converter-app-admin.php
    updating link

--- nasa-time-converter-app/admin/class-nasa-time-converter-app-admin.php
+++ nasa-time-converter-app/admin/class-nasa-time-converter-app-admin.php
@@ -3,7 +3,7 @@
 /**
  * The admin-specific functionality of the plugin.
  *
- * @link       https://www.ensembleconsultancy.com
+ * @link       https://github.com/EnsembleGovServices/TimeConvertorApp
  * @since      1.0.0
  *
  * @package    Nasa_Time_Converter_App
